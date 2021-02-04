4b6988e102d1c86b1896a2a7b426b28c1d679089
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-02-04 11:59:44 +0530

    Mobile home section added

--- src/app/app-header-mobile/home-mobile/home-mobile.component.ts
+++ src/app/app-header-mobile/home-mobile/home-mobile.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-mobile.component.css']
 })
 export class HomeMobileComponent implements OnInit {
-  carno = [1, 2, 3, 4, 5]
+  carno = [1, 2, 3, 4]
   blogBody = 'If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated. If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated'
 
   constructor() { }
