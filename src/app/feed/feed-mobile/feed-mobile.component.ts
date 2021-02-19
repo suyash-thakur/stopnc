04ca5a6ddb0bed97bf340bfb6dafce899599d107
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-02-19 12:48:34 +0530

    pagination mobile feed

--- src/app/feed/feed-mobile/feed-mobile.component.ts
+++ src/app/feed/feed-mobile/feed-mobile.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import {  Component,  HostListener, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { BlogService } from 'src/app/services/blog.service';
@@ -13,10 +14,15 @@ export class FeedMobileComponent implements OnInit {
   menuPosition: any;
   sticky: boolean;
   elementPosition: any;
-  blogBody = 'If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated. If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated'
+  hasNextpage: boolean;
+  currentPage = 0;
 
-  constructor(public router: Router, private route: ActivatedRoute, public blogServie: BlogService) {
-    this.route.data.subscribe(data => this.blogs = data);
+  constructor(public router: Router, private route: ActivatedRoute, public blogServie: BlogService, private http: HttpClient) {
+    this.route.data.subscribe((data) => {
+      this.blogs = data.blogs.docs;
+      this.hasNextpage = data.blogs.hasNextPage;
+      this.currentPage = this.currentPage + 1;
+    });
     console.log(this.blogs);
    }
 
@@ -36,13 +42,22 @@ export class FeedMobileComponent implements OnInit {
       return undefined;
     }
   }
+  onScroll() {
+    console.log("scroll");
+    if (this.hasNextpage) {
+      this.http.get('http://localhost:3000/api/blog/allBlog' + this.currentPage).subscribe((data: any) => {
+        console.log(data);
+        for (var i = 0; i < data.docs.length; i++) {
+          this.blogs.push(data.docs[i]);
+        }
+        this.currentPage = this.currentPage + 1;
+        this.hasNextpage = data.hasNextPage;
+      });
+    }
+
+  }
   @HostListener('window:scroll', ['$event'])
-  handleScroll(){
-    const windowScroll = window.pageYOffset;
-      if(windowScroll >= this.elementPosition){
-        this.sticky = true;
-      } else {
-          this.sticky = false;
-      }
+  handleScroll(event){
+    console.log("scroll");
   }
 }
