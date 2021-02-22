885a5c2f62560b37079976b3eb0348c291a80373
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-02-23 04:03:21 +0530

    Dynamic homepage desktop

--- src/app/desktop/home/main-one/main-one.component.ts
+++ src/app/desktop/home/main-one/main-one.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { BlogService } from 'src/app/services/blog.service';
 
 @Component({
@@ -7,11 +9,24 @@ import { BlogService } from 'src/app/services/blog.service';
   styleUrls: ['./main-one.component.css']
 })
 export class MainOneComponent implements OnInit {
-  blogBody = 'If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated. If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated'
+  blogBody = 'If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated. If you guys have been following me for a while, you know jeans are one of my favorite things to wear. I’m always on the search for the perfect-fitting pair of jeans, and I’ve got a few go-to trust-worthy brands. Additional Text to be truncated';
+  firstBlog: any;
+  secondBlog: any;
+  topBlogs: Array<any>;
+  isFirstLoaded = false;
 
-  constructor(public blogservice: BlogService) { }
+  constructor(public blogservice: BlogService, private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+    this.http.get('http://localhost:3000/api/admin/homepageInfo').subscribe((data: any) => {
+      console.log(data);
+      this.firstBlog = data.home.FirstBlog;
+      this.secondBlog = data.home.SecondBlog;
+      this.topBlogs = data.home.TopStories;
+      this.isFirstLoaded = true;
+    });
   }
-
+  blogClick(id) {
+    this.router.navigate(['/blog', id]);
+  }
 }
