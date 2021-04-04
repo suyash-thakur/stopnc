c2587aacf3f73afad5778161f327a9304dd7ddc3
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-04-05 03:41:05 +0530

    Email Mobile fixed

--- src/app/app-header-mobile/login-mobile/email-login-mobile/email-login-mobile.component.ts
+++ src/app/app-header-mobile/login-mobile/email-login-mobile/email-login-mobile.component.ts
@@ -12,6 +12,13 @@ export class EmailLoginMobileComponent implements OnInit {
 
   private authListenerSub: Subscription;
   private loggedin = false;
+  isWrongCred = false;
+  isEmailValid = true;
+  isNameValid = true;
+  isPasswordValid = true;
+ emailValidExpe : RegExp = new RegExp(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/);
+  nameValidExpe: RegExp = new RegExp(/^[A-Za-z]+$/);
+  passwordValidExpe: RegExp = new RegExp(/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/);
 
   constructor(public authService: AuthenticationService, public router: Router) { }
 
@@ -19,28 +26,53 @@ export class EmailLoginMobileComponent implements OnInit {
   }
 
   onLogin(email: string, password: string) {
-    this.authService.login(email, password);
-    this.authListenerSub = this.authService.getauthStatusListener().subscribe(
-      isAuthenticated => {
-      this.loggedin = isAuthenticated;
-      console.log(this.loggedin);
-      if (this.loggedin) {
-        this.authService.Userlogin = true;
-        this.router.navigate(['/']);
-      }
-    });
+    console.log( this.emailValidExpe.test(email));
+    if (!this.emailValidExpe.test(email)) {
+      this.isEmailValid = false;
+    } else {
+      this.isEmailValid = true;
+      this.authService.login(email, password);
+      this.authListenerSub = this.authService.getauthStatusListener().subscribe(
+        isAuthenticated => {
+          this.loggedin = isAuthenticated;
+          console.log(this.loggedin);
+          if (this.loggedin) {
+            this.authService.Userlogin = true;
+            this.router.navigate(['/']);
+          }
+        });
+    }
   }
   onSignup(email: string, password: string, firstname: string, lastname: string) {
     if (email === '' && password === '' && firstname === '') {
+
       window.alert('Please input correct value');
       return;
     } else {
-      const name = firstname + ' ' + lastname;
-      this.authService.createUser(email, password, name);
-      console.log(email);
-      console.log(password);
+      this.isPasswordValid = true;
+      this.isNameValid = true;
+      this.isEmailValid = true;
 
+      if (!this.nameValidExpe.test(firstname) && !this.nameValidExpe.test(lastname)) {
+        this.isNameValid = false;
+        return;
+      }
+      if (!this.emailValidExpe.test(email)) {
+        this.isEmailValid = false;
+        return;
+      }
+      if (!this.passwordValidExpe.test(password)) {
+        this.isPasswordValid = false;
+        return;
+      }
 
+      if (this.isEmailValid && this.isNameValid && this.isPasswordValid) {
+        const name = firstname + ' ' + lastname;
+        this.authService.createUser(email, password, name).then(res => {
+
+        });
+
+      }
   }
 }
 
